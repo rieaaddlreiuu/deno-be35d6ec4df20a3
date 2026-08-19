be0ff8d1d5fb0c678fb9cf7d1e1edc7bb624e869
rieaaddlreiuu <kg21428ks@isk.ed.jp>
2026-08-19 10:16:18 +0900

    aa

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ Deno.serve((req) => {
   console.log(pathname);
 
   if (req.method === "GET" && pathname === "/welcome-message") {
-    return new Response("jigインターンへようこそ！");
+    return new Response("これはなんですか？");
   }
 
   return serveDir(req, {
